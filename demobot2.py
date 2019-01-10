b1cdda01872a32b95685c1727c0aeb4b685a26a5
Ethan Gatchalian <egat7413@shared.sydney.edu.au>
2019-01-10 16:03:07 +1100

    Moved into github 1st time

---
+++ demobot2.py
@@ -0,0 +1,32 @@
+# Import flask
+from flask import Flask, request
+
+# Create your app (web server)
+app = Flask(__name__)
+
+
+# When people visit the home page '/' use the hello_world function
+@app.route('/')
+def hello_you():
+    return 'Hello, You!'
+
+@app.route('/ncss')
+def hello_NCSS():
+    return '<h1> NCSS </h1> NCSS'
+
+@app.route('/greet')
+def greet_person():
+    name = request.values.get('text')
+    return f'Hi {name}!'
+
+@app.route('/weather')
+def show_weather():
+    weather = request.values.get('text')
+    if int(weather) > 30:
+        return 'It\'s very hot.'
+    else:
+        return f"The temperature is {weather}."
+
+if __name__ == '__main__':
+    # Start the web server!
+    app.run(debug = True)
